Correct the calculation of centered and scaled coefficients; fix the error in minmax (range) scaling of data matrix; add tests for pretreatment; plot the jack-knife loadings using the variables used for model construction; update the readme; other modifications.

pypls/cross_validation.py
@@ -472,7 +472,7 @@ class CrossValidation:
         return self.estimator.corr
 
     @property
-    def covariance(self):
+    def covariance(self) -> np.ndarray:
         """
         Covariance
 
@@ -556,7 +556,7 @@ class CrossValidation:
         return self._mis_classifications
 
     @property
-    def used_variable_index(self):
+    def used_variable_index(self) -> np.ndarray:
         """
 
         Returns
@@ -568,7 +568,7 @@ class CrossValidation:
         return self.scaler.variable_index
 
     @property
-    def permutation_q2(self):
+    def permutation_q2(self) -> np.ndarray:
         """
 
         Returns
@@ -586,7 +586,7 @@ class CrossValidation:
         return self._perm_q2
 
     @property
-    def permutation_r2(self):
+    def permutation_r2(self) -> np.ndarray:
         """
 
         Returns
@@ -604,7 +604,7 @@ class CrossValidation:
         return self._perm_r2
 
     @property
-    def permutation_error(self):
+    def permutation_error(self) -> np.ndarray:
         """
 
         Returns
@@ -622,7 +622,7 @@ class CrossValidation:
         return self._perm_err
 
     @property
-    def correlation_permute_y(self):
+    def correlation_permute_y(self) -> np.ndarray:
         """
 
         Returns
@@ -640,14 +640,36 @@ class CrossValidation:
         return self._corr_y_perms
 
     @property
-    def vip(self):
+    def vip(self) -> np.ndarray:
         """
         Returns VIPs of variables
 
         """
         return self._vip
 
-    def p(self, metric="q2"):
+    @property
+    def num_samples(self) -> int:
+        """
+        Returns number of samples.
+
+        Returns
+        -------
+
+        """
+        return self._n
+
+    @property
+    def num_variables(self) -> int:
+        """
+        Returns number of samples.
+
+        Returns
+        -------
+
+        """
+        return self._p
+
+    def p(self, metric="q2") -> float:
         """
         Calculates the significance of the constructed model by
         permutation test.
@@ -727,9 +749,10 @@ class CrossValidation:
 
         # coefficients of centered and scaled X
         estimator, _ = self._create_scaler_estimator()
-        scaler = pretreatment.Scaler("uv")
-        x_uv = scaler.fit(x[:, self.scaler.variable_index], y)
-        estimator.fit(x_uv.copy(), y.copy(), npc)
+        scaler = pretreatment.Scaler(scaler="uv", fit_type="balanced")
+        ys = (y - y.mean()) / y.std()
+        x_uv = scaler.fit(x[:, self.scaler.variable_index], ys)
+        estimator.fit(x_uv.copy(), ys.copy(), npc)
         self._coefcs = estimator.coefs[self._opt_component]
 
     def _reset_y(self, y) -> np.ndarray:

pypls/plotting.py
@@ -192,9 +192,9 @@ class Plots:
         loading_mean = self._model.loadings_cv.mean(axis=0)
         loading_std = self._model.loadings_cv.std(axis=0)
         # unzero mean and standard deviation
-        val_ix: np.ndarray = (loading_mean == 0.) & (loading_std == 0.)
-        loading_mean = loading_mean[~val_ix]
-        loading_std = loading_std[~val_ix]
+        val_ix: np.ndarray = self._model.used_variable_index
+        loading_mean = loading_mean[val_ix]
+        loading_std = loading_std[val_ix]
         # critical value
         t_critic = stats.t.ppf(1 - (alpha / 2), self._model.kfold - 1)
         # jackknife confidence interval

pypls/pretreatment.py
@@ -4,7 +4,7 @@ Preprocess data matrix.
 import numpy as np
 from typing import Optional
 
-from .core import scale_x_class
+from .core import scale_x_class, scale_x_reg
 
 
 class Scaler:
@@ -17,10 +17,15 @@ class Scaler:
         Method for scaling, "uv" for unit variance, "pareto" for
         pareto scaling, "mean" for mean scaling and "minmax" for
         minmax scaling. Default is "pareto".
+    fit_type: str
+        Whether scale data matrix based on the distribution of y
+        labels.
+            Unbalanced: The classes are unbalanced.
+            Balanced: The classes are balanced.
 
     """
 
-    def __init__(self, scaler="pareto"):
+    def __init__(self, scaler="pareto", fit_type: str = "unbalanced"):
         if scaler == "mean":
             self.scaler_tag: int = 1
         elif scaler == "pareto":
@@ -29,12 +34,18 @@ class Scaler:
             self.scaler_tag: int = 3
         elif scaler == "minmax":
             self.scaler_tag: int = 4
+        else:
+            raise ValueError("Expected Scaler type 'uv', 'pareto', 'mean' or "
+                             f"'minmax', got {scaler}.")
+
+        self.fit_balanced: bool = fit_type == "balanced"
 
         self._center: Optional[np.ndarray] = None
         self._normalizer: Optional[np.ndarray] = None
         self._var_index: Optional[np.ndarray] = None
 
-    def fit(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def fit(self, x: np.ndarray, y: np.ndarray)\
+            -> np.ndarray:
         """
         Fits a scaler model.
 
@@ -50,7 +61,10 @@ class Scaler:
 
         """
         xs: np.ndarray = np.ascontiguousarray(x.copy())
-        xs, ix, center, normalizer = scale_x_class(xs, y, self.scaler_tag)
+        if not self.fit_balanced:
+            xs, ix, center, normalizer = scale_x_class(xs, y, self.scaler_tag)
+        else:
+            xs, ix, center, normalizer = scale_x_reg(xs, self.scaler_tag)
         self._center = center
         self._normalizer = normalizer
         self._var_index = ix